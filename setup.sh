5ba683605f01ba01c0e306752a7fbd73d385950f
shearn89 <alexander.shearn@gmail.com>
2017-06-29 12:26:37 +0100

    Shuffle

--- setup.sh
+++ setup.sh
@@ -6,27 +6,28 @@ sudo apt install vim
 # Install git
 sudo apt install git
 
-# Install hub
-xdg-open https://github.com/github/hub/releases/latest
-
 # Install pathogen
 mkdir -p ~/.vim/autoload ~/.vim/bundle && \
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 # Install vim-sensible
 cd ~/.vim/bundle
-git clone git://github.com/tpope/vim-sensible.git
+git clone https://github.com/tpope/vim-sensible.git
 
-git clone tpope/vim-unimpaired
-git clone tpope/vim-markdown
-git clone tpope/vim-surround
-git clone tpope/vim-characterize
-git clone tpope/vim-commentary
-git clone tpope/vim-abolish
-git clone altercation/vim-colors-solarized
+git clone https://github.com/tpope/vim-unimpaired
+git clone https://github.com/tpope/vim-markdown
+git clone https://github.com/tpope/vim-surround
+git clone https://github.com/tpope/vim-characterize
+git clone https://github.com/tpope/vim-commentary
+git clone https://github.com/tpope/vim-abolish
+git clone https://github.com/altercation/vim-colors-solarized
 
 mkdir -p ~/repos ~/source
 cd ~/source
-git clone Anthony25/gnome-terminal-colors-solarized
-git clone seebi/dircolors-solarized
+git clone https://github.com/Anthony25/gnome-terminal-colors-solarized
+git clone https://github.com/seebi/dircolors-solarized
 
+# Install hub
+echo "please install hub from https://github.com/github/hub/releases/latest"
+
+cp vimrc ~/.vimrc
